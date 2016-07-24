444f00b589e69b42855db959a8af8ba0aaaba8dd
Meghdad Hadidi <meghdad.1987@gmail.com>
2016-07-24 23:59:49 +0430

    Update Foursquare.js

--- include/js/Foursquare.js
+++ include/js/Foursquare.js
@@ -1,3 +1,42 @@
-var Foursquare = function () {
-    this.type = "$11";
-};
+// Adding Foursquare Constructor to Shapes Mediator
+
+ShapesMediator.addShape('Foursquare', function (...sides) {
+    
+	this.name = 'Foursquare';
+	
+	this.vertices = ['A', 'B', 'C', 'D'];
+	this.sides = sides;
+
+	// Returns Foursquare Type
+	this.getType = function(){
+		// Method to return Foursquare's type
+	};
+
+
+	// Returns Foursquare Perimeter
+	this.getPerimeter = function(){
+		let [a, b, c, d] = this.sides;
+		let perimeter = parseInt(a) + parseInt(b) + parseInt(c) + parseInt(d);
+
+		logger.show('info', 'Foursquare perimeter is <strong>"'+ perimeter + '"</strong> !');
+		return perimeter;
+	};
+
+	this.getArea = function(){
+		let p = this.getPerimeter();
+		let area = parseInt(parseInt(sides[0]) * parseInt(sides[1]));
+
+		logger.show('info', 'Foursquare area is <strong>"'+ area + '"</strong> !');
+		return area;
+	};
+
+
+	return {
+		name: this.name,
+		type: this.getType(),
+		perimeter: this.getPerimeter(),
+		area: this.getArea(), 
+		sides: this.sides
+	}
+
+});
